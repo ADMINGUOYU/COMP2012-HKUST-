dfdd24b3baea1513c6fd27602e10fe679fb7b59a
GUO Yu <111267796+ADMINGUOYU@users.noreply.github.com>
2025-03-09 13:06:32 +0000

    Upload all ClsCode / ClsCode sub-folders renamed

---
+++ ClsCode/Namespace (Self-study)/std-per-instance-using.cpp
@@ -0,0 +1,14 @@
+#include <iostream>     /* File: std-per-instance-using.cpp */
+using namespace std;
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+    std::cout << s << std::endl;
+
+    s += " is good!";
+    std::cout << s << std::endl;
+    
+    return 0;
+}
